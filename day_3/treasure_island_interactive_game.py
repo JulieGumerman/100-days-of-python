05231fa9625821eb7bd6a584a5deab141d94ac01
Julie Gumerman <julie.gumerman@gmail.com>
2024-02-04 18:13:03 -0700

    reviewing if statements in Python

---
+++ day_3/treasure_island_interactive_game.py
@@ -0,0 +1,20 @@
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+direction_1 = input("Would you like to go left or right?\n")
+
+if direction_1 == "left":
+    print("next step, ya")
+else:
+    print("Fall into a hole. \nGame over")
+#if left, keep going
+
+#input("Swim or wait?")
+#if swim or anything else, print ("Attacked by trout. Game over.")
+
+#if wait, keep going
+
+#input("Which color door: red, yellow or blue?")
+#if red, print("You just burned up! Game over.")
+#if yellow, print("You just found the treasure.")
+#if blue, print("You got eaten by beasts. Tough luck bruh.")
+
