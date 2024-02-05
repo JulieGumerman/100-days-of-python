64f76100bcd393f97f655c0ec6d19161f179d167
Julie Gumerman <julie.gumerman@gmail.com>
2024-02-04 17:58:38 -0700

    data type review

---
+++ day_2/data_type_conversion.py
@@ -0,0 +1,4 @@
+two_digit_number = input("Put in a two-digit number\n")
+number_1 = int(two_digit_number[0])
+number_2 = int(two_digit_number[1])
+print(f"The sum of the two numbers is {number_1 + number_2}")
