ba6b4abf34ac4a1391c776dd85e65ed504122993
Julie Gumerman <julie.gumerman@gmail.com>
2024-02-15 17:55:07 -0700

    fixed the coin-toss game

--- day_4/coin_toss.py
+++ day_4/coin_toss.py
@@ -1,18 +1,26 @@
 import random
 
 users_choice = input ("Heads or tails? \n")
+print(f"You chose {users_choice}")
 
-result = random.seed(2)
-print(result)
+
+
+coin_toss_result = random.randint(0,1)
+
+if coin_toss_result == 0:
+    print("The coin landed on heads.")
+elif coin_toss_result == 1:
+    print("The coin landed on tails.")
+
 
 choice_as_number = 3
-if input == "heads":
+if users_choice == "heads":
     choice_as_number = 0
-elif input == "tails":
+elif users_choice == "tails":
     choice_as_number = 1
 
 
-if choice_as_number == result:
+if choice_as_number == coin_toss_result:
     print("you win")
 else:
     print("you lose")
