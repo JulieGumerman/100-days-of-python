b9fb92fd021d6a362917726287d8f57a07b69f7d
Julie Gumerman <julie.gumerman@gmail.com>
2024-03-26 20:36:14 -0600

    beginning of 'rock paper scissors'

--- day_4/rock_paper_scissors.py
+++ day_4/rock_paper_scissors.py
@@ -1,1 +1,13 @@
-print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors")
+user_input = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors")
+
+user_input_integer = int(user_input)
+if user_input_integer == 0:
+    print("You chose ROCK.")
+elif user_input_integer == 1:
+    print("You chose PAPER")
+elif user_input_integer == 2:
+    print("You chose SCISSORS")
+else:
+    print("Choose a real answer")
+
+    
