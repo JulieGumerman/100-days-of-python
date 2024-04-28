9ff40b573f5224385890db7f82948d809fc6b802
Julie Gumerman <julie.gumerman@gmail.com>
2024-04-27 18:58:15 -0600

    derp de derp

--- day_5/high_score.py
+++ day_5/high_score.py
@@ -3,9 +3,9 @@ for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
 
 high_score = 0
-for n in range(0, len(student_scores)):
-    if high_score < student_scores[n]:
-        high_score = student_scores[n]
+for score in student_scores:
+    if high_score < score:
+        high_score = score
 
 if high_score <= 0:
     print(f"These students have anti-student test scores. The highest score was {high_score}!")
