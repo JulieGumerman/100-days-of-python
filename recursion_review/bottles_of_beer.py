fd1a7d36d53af51486308ec1ab6cf54173f6d358
Julie Gumerman <julie.gumerman@gmail.com>
2024-11-16 14:24:46 -0700

    nevermind. it was right the first time. It just helps if you CALL your function.

--- recursion_review/bottles_of_beer.py
+++ recursion_review/bottles_of_beer.py
@@ -1,9 +1,12 @@
 def bottles_of_beer(bottle_number):
-    print(f"{bottle_number} bottles of beer on the wall./n {bottle_number} bottles of beer. Take one down, pass it around.")
+    print(f"{bottle_number} bottles of beer on the wall.\n {bottle_number} bottles of beer. \nTake one down, pass it around.")
     if bottle_number < 1:
-        print("Congrats. You counted down. /nYou are a legend among men.")
+        print("Congrats. You counted down. \nYou are a legend among men.")
         return
     else:
         new_bottle_number = bottle_number - 1
         print(f"{new_bottle_number} bottles of beer on the wall.")
         bottles_of_beer(new_bottle_number)
+
+
+bottles_of_beer(99)
