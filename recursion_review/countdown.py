794ea3f165647dee99201bf633aa67df7bc9f895
Julie Gumerman <julie.gumerman@gmail.com>
2024-11-16 14:15:33 -0700

    and then I got an infinite loop. Ooops.

--- recursion_review/countdown.py
+++ recursion_review/countdown.py
@@ -7,7 +7,7 @@ def countdown(i):
     # The "else" section calls the outer function, with an integer one less than the one the outer function called.
     # However, there is always one lower number, so you need a base case.
     # Enter the "if" section. If the number is less than one, game over. You exit the loop, and it returns.
-    
+
     if i < 1:
         print("Yay!!!! No infinite loop!")
         return
