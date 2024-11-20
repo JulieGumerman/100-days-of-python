0f49f9bf5d0ceca1cd69dfe29ab58caba5fcaa89
Julie Gumerman <julie.gumerman@gmail.com>
2024-11-19 18:35:21 -0700

    more turtle mandalas

--- day_16:_OOP/turtle_mandala.py
+++ day_16:_OOP/turtle_mandala.py
@@ -3,7 +3,7 @@ from turtle import *
 trinity = Turtle()
 
 for steps in range(100):
-    for c in ("blue","red","green"):
+    for c in ("cyan4","DeepPink","DarkViolet"):
         color(c)
         forward(steps)
         right(30)
