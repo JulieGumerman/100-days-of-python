0e38d0bae464123421ddb78c70e2fd5ef903baf5
Julie Gumerman <julie.gumerman@gmail.com>
2024-11-24 15:56:11 -0700

    left-alignment to table

--- day_16:_OOP/nepal_religious_demographics.py
+++ day_16:_OOP/nepal_religious_demographics.py
@@ -12,4 +12,5 @@ table.add_row(["Prakriti (nature worship)",.5])
 
 table.add_column("ethnic breakdown", ["majority of all Nepali ethnic groups", "Sherpa, Dolpa, Lopa and other ethnic groups of the mountain regions","Madhesh Muslims (of Middle Eastern descent)","Kirati","n/a","unknown"])
 
+table.align = "l"
 print(table)
