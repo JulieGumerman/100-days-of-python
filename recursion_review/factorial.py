2a22ff5d30369ffa3949aa9a5225f2be4ccd45dc
Julie Gumerman <julie.gumerman@gmail.com>
2024-12-08 12:11:47 -0700

    recursive function that actually multiplies to get the factorial rather than just counting down

--- recursion_review/factorial.py
+++ recursion_review/factorial.py
@@ -19,15 +19,16 @@ def factorial(x):
 # However, it doesn't actually return the factorial product. It just counts it down. What happens if I print it out?
 
 def factorialPrinted(x,y):
+    # The variable y holds the factorial product as we are multiplying the different integers leading from x down to 1.
     if x == 1:
         # Base case: stop! You're down to 1.
         print("final", y)
         return 1
     else:
-        y = x * (x - 1)
-        print(x, "--->   ", y)
+        # To keep multiplying, we multiply y by each integer smaller than x, one by one, as we count down from x to 1
+        y = y * x
         #take the current input and multiply it by the next lowest number
         return x * factorialPrinted((x-1), y)
 
 
-factorialPrinted(5,0)
+factorialPrinted(5,1)
